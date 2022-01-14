ec0ecd1935b01286e412eecbd4fbabe5c2af0ee4
Mary Margaret <mmlawton15@gmail.com>
2022-01-14 10:23:49 -0500

    refactored code and finished module 11

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 const PORT = process.env.PORT || 3001;
 const app = express();
-//parse incoming string or array data
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-//parse incoming JSON data
 app.use(express.json());
 app.use(express.static('public'));
-const {animals} = require('./data/animals');
+
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-})
+  console.log(`API server now on port ${PORT}!`);
+});
